5a1ee01742b2172c01c885786b9edad99528e5e0
gagan <gaganparmar110@gmail.com>
2019-07-14 22:58:12 +0530

    fabonaci code

---
+++ src/fabonacciSeries/fabonacciSeries.java
@@ -0,0 +1,18 @@
+package fabonacciSeries;
+
+import java.util.Scanner;
+
+public class fabonacciSeries {
+    public static void main(String[] args) {
+        System.out.println("enter a no: ");
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int p=0,q=1,sum;
+        for (int i=0;i<a;i++){
+            sum=p+q;
+            p=q;
+            q=sum;
+            System.out.print(+sum+" ");
+        }
+    }
+}
